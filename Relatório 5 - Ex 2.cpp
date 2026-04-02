4fb854e42b2b29d1791099d04199e620abc9fe1f
PedroGasp <pedro.gasparotto.c@gmail.com>
2026-04-02 20:06:33 -0300

    Add array multiplication program in C++

---
+++ Relatório 5 - Ex 2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+float multiplicaArray(float arr[], int tamanho) {
+    float produto = 1;
+
+    for (int i = 0; i < tamanho; i++) {
+        produto *= arr[i];
+    }
+
+    return produto;
+}
+
+int main() {
+    int tamanho;
+
+    cout << "Tamanho do array? ";
+    cin >> tamanho;
+
+    float arr[tamanho];
+    for (int i = 0; i < tamanho; i++) {
+        cout << "Digite o valor " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+
+    float resultado = multiplicaArray(arr, tamanho);
+
+    cout << "Resultado: " << resultado << endl;
+
+    return 0;
+}
