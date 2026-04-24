fe905c57dd36c9a39bb1c3abdcf75b2b6a4689e5
PedroGasp <pedro.gasparotto.c@gmail.com>
2026-04-23 23:49:11 -0300

    Add Personagem and Cavaleiro classes

---
+++ Relatório 8 - Ex 1.py
@@ -0,0 +1,38 @@
+class Personagem:
+    def __init__(self, vida, resistencia):
+        self._vida = vida
+        self._resistencia = resistencia
+
+    @property
+    def vida(self):
+        return self._vida
+
+    @vida.setter
+    def vida(self, valor):
+        if valor >= 0:
+            self._vida = valor
+        else:
+            print("Vida não pode ser negativa.")
+
+    def __str__(self):
+        return f"Personagem com {self._vida} de vida e {self._resistencia} de resistência"
+
+
+class Cavaleiro(Personagem):
+    def __init__(self, vida, resistencia, armadura_pesada):
+        super().__init__(vida, resistencia)
+        self.armadura_pesada = armadura_pesada
+
+    def __str__(self):
+        tipo_armadura = "com armadura pesada" if self.armadura_pesada else "sem armadura pesada"
+        return f"Cavaleiro com {self._vida} de vida, {self._resistencia} de resistência e {tipo_armadura}"
+
+
+p = Personagem(100, 50)
+print(p)
+
+c = Cavaleiro(150, 80, True)
+print(c)
+
+c.vida = 120
+print(c.vida)
